a2f46d453bba654dfb9ea2c6c53967720fab6d0a
angelsystem <angelsystem1387@gmail.com>
2016-06-04 16:50:25 -0500

    first commit

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,7 +5,7 @@ aws = {
   access_key_id: "AKIAIZE4N7LF6BXERSYQ",
   secret_access_key: "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",
   region: "us-west-2",
-  bucket: "weclubrefile",
+  bucket: "weclubdemo3",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
