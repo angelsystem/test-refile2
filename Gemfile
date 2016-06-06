b3897117d052f0debd3ec0a0a7db03699fd47b58
Mario J. Inga Cahuana <mario21ic@gmail.com>
2016-06-06 01:18:52 -0500

    fix(post) ya retorna la url correcta del s3 publico

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Unicorn as the app server
 # gem 'unicorn'
-gem 'pg'
+#gem 'pg'
 gem 'pry'
 gem 'pry-rails'
 gem 'simple_form'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,17 +18,17 @@
 
 
 $(document).on("upload:start", "form", function(e) {
-  $(this).find("input[type=submit]").attr("disabled", true)
+  $(this).find("input[type=submit]").attr("disabled", true);
 });
 
-$(document).on("upload:complete", "form", function(e) {
+$(document).on("upload:success", "form", function(e) {
   if(!$(this).find("input.uploading").length) {
-    $(this).find("input[type=submit]").removeAttr("disabled")
+    $(this).find("input[type=submit]").removeAttr("disabled");
   }
   
-  image_info = $("input[name='user[image]']").val()
-  image_id = JSON.parse(image_info).id
+  image_info = $("input[name='post[pictures_attributes][0][file]']").val();
+  image_id = JSON.parse(image_info).id;
 
-  url = "https://s3-us-west-2.amazonaws.com/weclubdemo3/cache" + image_id
-  $("#image").html($("<img />").attr({src: url}))
+  url = "https://s3-us-west-2.amazonaws.com/weclubdemo3/cache/" + image_id;
+  $("#image").html($("<img />").attr({src: url}));
 });
